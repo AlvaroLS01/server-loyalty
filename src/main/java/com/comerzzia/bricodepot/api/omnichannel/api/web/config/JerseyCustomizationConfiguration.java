fb35c1e03803fa9c5ee22e33c67382e6ec140202
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-10-23 15:51:32 +0200

    Replace Jersey resource registration to avoid duplicates

--- src/main/java/com/comerzzia/bricodepot/api/omnichannel/api/web/config/JerseyCustomizationConfiguration.java
+++ src/main/java/com/comerzzia/bricodepot/api/omnichannel/api/web/config/JerseyCustomizationConfiguration.java
@@ -1,11 +1,8 @@
 package com.comerzzia.bricodepot.api.omnichannel.api.web.config;
 
-import org.springframework.boot.autoconfigure.jersey.ResourceConfigCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.comerzzia.api.omnichannel.web.rest.salesdoc.SalesDocumentResource;
-import com.comerzzia.bricodepot.api.omnichannel.api.web.rest.salesdoc.BricodepotSalesDocumentResource;
 import com.comerzzia.bricodepot.api.omnichannel.api.web.salesdocument.DocumentoVentaImpresionFilter;
 import com.comerzzia.bricodepot.api.omnichannel.api.web.salesdocument.DocumentoVentaImpresionServicio;
 
@@ -16,21 +13,5 @@ public class JerseyCustomizationConfiguration {
     public DocumentoVentaImpresionFilter documentoVentaImpresionFilter(
             DocumentoVentaImpresionServicio servicioImpresion) {
         return new DocumentoVentaImpresionFilter(servicioImpresion);
-    }
-
-    @Bean
-    public ResourceConfigCustomizer documentoVentaImpresionFilterCustomizer(
-            DocumentoVentaImpresionFilter documentoVentaImpresionFilter) {
-        return resourceConfig -> resourceConfig.register(documentoVentaImpresionFilter);
-    }
-
-    @Bean
-    public ResourceConfigCustomizer bricodepotSalesDocumentResourceCustomizer(
-            BricodepotSalesDocumentResource bricodepotSalesDocumentResource) {
-        return resourceConfig -> {
-            resourceConfig.getClasses().removeIf(SalesDocumentResource.class::equals);
-            resourceConfig.getSingletons().removeIf(instance -> SalesDocumentResource.class.equals(instance.getClass()));
-            resourceConfig.register(bricodepotSalesDocumentResource);
-        };
     }
 }
