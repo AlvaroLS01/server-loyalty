7b3a9a9912c10867a6b1b9c0f2111f1310ab1476
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-10-23 14:19:21 +0200

    Fix Jasper print service override

---
+++ src/main/java/com/comerzzia/bricodepot/api/omnichannel/api/web/config/JerseyCustomizationConfiguration.java
@@ -0,0 +1,31 @@
+package com.comerzzia.bricodepot.api.omnichannel.api.web.config;
+
+import org.springframework.boot.autoconfigure.jersey.ResourceConfigCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.comerzzia.bricodepot.api.omnichannel.api.web.rest.salesdoc.BricodepotSalesDocumentResource;
+import com.comerzzia.bricodepot.api.omnichannel.api.web.salesdocument.DocumentoVentaImpresionFilter;
+import com.comerzzia.bricodepot.api.omnichannel.api.web.salesdocument.DocumentoVentaImpresionServicio;
+
+@Configuration
+public class JerseyCustomizationConfiguration {
+
+    @Bean
+    public DocumentoVentaImpresionFilter documentoVentaImpresionFilter(
+            DocumentoVentaImpresionServicio servicioImpresion) {
+        return new DocumentoVentaImpresionFilter(servicioImpresion);
+    }
+
+    @Bean
+    public ResourceConfigCustomizer documentoVentaImpresionFilterCustomizer(
+            DocumentoVentaImpresionFilter documentoVentaImpresionFilter) {
+        return resourceConfig -> resourceConfig.register(documentoVentaImpresionFilter);
+    }
+
+    @Bean
+    public ResourceConfigCustomizer bricodepotSalesDocumentResourceCustomizer(
+            BricodepotSalesDocumentResource bricodepotSalesDocumentResource) {
+        return resourceConfig -> resourceConfig.register(bricodepotSalesDocumentResource);
+    }
+}
