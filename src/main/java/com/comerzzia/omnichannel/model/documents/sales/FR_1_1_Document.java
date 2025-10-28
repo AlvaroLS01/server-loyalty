38ff04a242c1af697a27d4ef8e653b208dbc4dd9
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-10-28 13:32:02 +0100

    Fix FR converter to use ticket model

---
+++ src/main/java/com/comerzzia/omnichannel/model/documents/sales/FR_1_1_Document.java
@@ -0,0 +1,12 @@
+package com.comerzzia.omnichannel.model.documents.sales;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.comerzzia.omnichannel.model.documents.sales.ticket.TicketVentaAbono;
+
+@XmlRootElement(name = "ticket")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class FR_1_1_Document extends TicketVentaAbono {
+}
